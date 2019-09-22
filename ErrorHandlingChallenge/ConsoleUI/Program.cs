a0424f38e165c58bacc13b06138364d7c4032ff8
Lukas Lapinskas <51517974+LapinskasL@users.noreply.github.com>
2019-09-22 15:57:42 -0500

    completed Error Handling challenge
    https://www.iamtimcorey.com/courses/c-weekly-challenges/lectures/6877624

---
+++ ErrorHandlingChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+                    Console.WriteLine(result.TransactionAmount);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is IndexOutOfRangeException)
+                    {
+                        Console.Write("Skipped invalid record");
+                    }
+                    else if (ex is FormatException && i != 5)
+                    {
+                            Console.Write("Formatting Issue");
+                    }
+                    else if (ex is NullReferenceException)
+                    {
+                        Console.Write("Null value for item " + i);
+                    }
+                    else
+                    {
+                        Console.Write("Payment skipped for payment with " + i + " items");
+                    }
+
+                    // write InnerException message, if exists
+                    if (ex.InnerException != null)
+                    {
+                        Console.Write(" " + ex.InnerException.Message);
+                    }
+
+                    Console.Write("\n"); // new line
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
