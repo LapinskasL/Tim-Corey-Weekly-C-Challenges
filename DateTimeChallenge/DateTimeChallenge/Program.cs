8de934ab24bb99bcf5656014ba18892d0f8c63e3
Lukas Lapinskas <51517974+LapinskasL@users.noreply.github.com>
2019-09-22 20:33:10 -0500

    Add files via upload
    https://www.iamtimcorey.com/courses/c-weekly-challenges/lectures/
    
    Formats to try for date:
    M/dd/yyyy   (example: 9/22/2019)
    dd/MM/yy   (example: 22/09/19)
    
    Formats to try for time:
    h:mm tt   (example: 8:30 PM)
    H:mm      (example: 20:30)

---
+++ DateTimeChallenge/DateTimeChallenge/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a date: ");
+            string dateInput = Console.ReadLine();
+            Console.Write("Enter date format: ");
+            string dateformat = Console.ReadLine();
+
+            DateTime pastDate = DateTime.ParseExact(dateInput, dateformat,
+                                             CultureInfo.InvariantCulture);
+
+            DateTime currentDate = DateTime.Now;
+
+            var daysAgo = (currentDate - pastDate).TotalDays;
+
+            Console.WriteLine("It has been " + Math.Floor(daysAgo) + " days since " + dateInput + ".");
+            Console.Write(Environment.NewLine);
+
+
+
+
+
+            Console.Write("Enter a time: ");
+            string timeInput = Console.ReadLine();
+            Console.Write("Enter time format: ");
+            string timeFormat = Console.ReadLine();
+
+            DateTime pastTime = DateTime.ParseExact(timeInput, timeFormat,
+                                             CultureInfo.InvariantCulture);
+
+            DateTime currentTime = DateTime.Now;
+
+            if (pastTime > currentTime)
+            {
+               pastTime = pastTime.AddDays(-1);
+            }
+
+            var hoursAgo = (currentTime - pastTime);
+            var minutesAgo = (hoursAgo.TotalHours - Math.Floor(hoursAgo.TotalHours)) * 60;
+
+            Console.WriteLine(timeInput + " was " +
+                Math.Floor(hoursAgo.TotalHours) + " hours and " + Math.Floor(minutesAgo) + " minutes ago.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
